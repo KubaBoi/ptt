9466acdc9155b4b2676bd4a815fea3c6c8f20cca
KubaBoi <jakubanderle@outlook.cz>
2022-11-14 11:32:05 -0600

    wor

---
+++ .github/promote.py
@@ -0,0 +1,22 @@
+
+import os
+
+path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), "..", "ptt.py"))
+
+with open(path, "r") as f:
+	lines = f.readlines()
+	
+old_version = ""
+for line in lines:
+	if (line.startswith("version:")):
+		vers = line.split(" ")[-1].strip().split(".")
+		old_version = ".".join(vers)
+		v = int(vers[2]) + 1
+		vers[2] = str(v)
+		print("Promoting version:", ".".join(vers))
+		
+with open(path, "r") as f:
+	content = f.read()
+with open(path, "w") as f:
+	f.write(content.replace(old_version, ".".join(vers)))
+	
