24d3b57ecec4557d4352b036f9a7e5f713f595da
GitHub Action <action@github.com>
2022-12-01 21:58:52 +0000

    Promoting version: 0.5.38

--- src/managerClasses.py
+++ src/managerClasses.py
@@ -26,7 +26,7 @@ $(NAME): $(OBJFILES)
 	"""
 
 class V:
-	VERSION = "0.5.37"
+	VERSION = "0.5.38"
 
 class C:
 	HEADER = '\033[95m'
--- src/ptt.py
+++ src/ptt.py
@@ -2,7 +2,7 @@
 """
 Test tool for ProgTests from CVUT FIT
 
-version: 0.5.37
+version: 0.5.38
 """
 __docformat__ = "reStructedText"
 
